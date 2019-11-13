5db1c69d302e08614db5ed92b76f3b25d4eede26
nic <nicsiebenborn@gmail.com>
2019-11-13 20:53:28 +0100

    made slave software for arduino; no master to test as jenny droped out

--- ArduinoSW/mcu/mcu.ino
+++ ArduinoSW/mcu/mcu.ino
@@ -1,10 +1,10 @@
 #include <ESP8266WiFi.h>
-
+#include<SPI.h>
 
 // Replace with your network credentials
 const char* ssid = "App4UCU";
 const char* password = "WIFIPASSWORD";
-
+volatile byte Slavereceived
 
 
 String page = "";
@@ -19,9 +19,13 @@ void setup(void){
     delay(500);
     Serial.print(".");
   }
- 
+ SPI.attachInterrupt();
 }
- 
+ ISR (SPI_STC_vect){                       //Inerrrput routine function 
+  Slavereceived = SPDR;                   // Value received from master if store in variable slavereceived
+  Serial.print(Slavereceived);
+}
+
 void loop(void){
   
 }
