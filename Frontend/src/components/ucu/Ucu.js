0841bc54769057bea539c6ded43413ee9dd5cdb7
freaksN <r0g3r@abv.bg>
2020-01-25 17:17:46 +0100

    Cleanup, refactoring and adding comments for understaing the functions. Also user story 56

--- Frontend/src/components/ucu/Ucu.js
+++ Frontend/src/components/ucu/Ucu.js
@@ -33,42 +33,48 @@ const sliderFloatValues = [
     },
 ];
 
-
-
-
 export default function Ucu() {
 
+    //States
     const [inputFieldInteger, setInputFieldInteger] = useState(44);
-    const [sliderValue, setSliderValue] = useState(30);
+    const [sliderIntegerValue, setSliderIntegerValue] = useState(30);
     const [switchButton, setSwitchButton] = useState(true);
-    const [someFloatValue, setSomeFloatValue] = useState(1.5);
+    const [sliderFloatValue, setSliderFloatValue] = useState(1.5);
     const [xmlUpload, setXmlUpload] = useState(null);
-    const [showElement, setShowElement] = useState(false);
+    const [showSwitchButton, setShowSwitchButton] = useState(false);
+    const [showInputField, setShowInputField] = useState(false);
     const [showFloatSlider, setShowFloatSlider] = useState(false);
     const [showIntegerSlider, setShowIntegerSlider] = useState(false);
 
+
+
+    //it is called whenever something is uploaded and saves the values of the uploaded file to the xmlUpload state
     function uploadXMLHandler(e) {
         setXmlUpload(e.target.files[0]);
     }
 
-
-
+    //Handles XML show/hide elements
     useEffect(() => {
         if (xmlUpload != null) {
             var fileReader = new FileReader();
             var tmp = false;
             fileReader.onload = () => {
                 if (fileReader.result.includes("test_b")) {
-                    setShowElement(true);
+                    setShowSwitchButton(true);
                 } else {
-                    setShowElement(false);
+                    setShowSwitchButton(false);
                 }
                 if (fileReader.result.includes("test_r32")) {
                     setShowFloatSlider(true);
                 } else {
                     setShowFloatSlider(false);
                 }
-                ["test_u8","test_i8","test_u16","test_i16","test_u32","test_i32"].forEach(elem => { if(fileReader.result.includes(elem)) tmp = true})
+                if (fileReader.result.includes("test_u8") || fileReader.result.includes("test_i8")) {
+                    setShowInputField(true);
+                } else {
+                    setShowInputField(false);
+                }
+                ["test_u16", "test_i16", "test_u32", "test_i32"].forEach(elem => { if (fileReader.result.includes(elem)) tmp = true })
                 if (tmp) {
                     setShowIntegerSlider(true);
                 } else {
@@ -80,10 +86,9 @@ export default function Ucu() {
         }
     });
 
-
-
-
-
+    //async functions for frontend <--> backend values GET/PUT requests.
+    //Any change on the WebApp will be directly sent to the backed.
+    //Going to IPAddress/sb, /sv,  /sf, /ifv will display/print the value coming from the backend on the WebApp.
     async function fetchSwitchButtonData() {
         fetch('/sb')
             .then(res => res.text())
@@ -96,52 +101,79 @@ export default function Ucu() {
             .then(res => res.text())
     }
 
-
-
-
-    async function fetchSliderValueData() {
+    async function fetchIntegerSliderValueData() {
         fetch('/sv')
             .then(res => res.text())
     }
 
-    async function fetchSliderValuePutData() {
-        let loopbackValue = sliderValue.toString();
+    async function fetchIntegerSliderValuePutData() {
+        let loopbackValue = sliderIntegerValue.toString();
         fetch('/sv', {
             method: 'PUT', body: loopbackValue
         })
             .then(res => res.text())
     }
 
+    async function fetchFloatSliderValueData() {
+        fetch('/sf')
+            .then(res => res.text())
+    }
+
+    async function fetchFloatSliderIntegerValuePutData() {
+        fetch('/sf', {
+            method: 'PUT', body: sliderFloatValues.toString()
+        })
+            .then(res => res.text())
+    }
+    async function fetchInputFieldValueData() {
+        fetch('/ifv')
+            .then(res => res.text())
+    }
+
+    async function fetchInputFieldValuePutData() {
+        fetch('/ifv', {
+            method: 'PUT', body: inputFieldInteger.toString()
+        })
+            .then(res => res.text())
+    }
 
     function handleNormalFieldInteger(event, value) {
         setInputFieldInteger(event.target.value);
     }
 
-    function handleChange(event, value) {
-        setSliderValue(value);
+    function handleSliderIntegerValue(event, value) {
+        setSliderIntegerValue(value);
     }
 
-    useEffect(() => {
-        console.log("Switch button is turned " + (switchButton ? 'on' : 'off'));
-        console.log('Slider value ' + sliderValue);
-        fetchSwitchButtonData();
-        fetchSwitchButtonPutData();
-        fetchSliderValueData();
-        fetchSliderValuePutData();
-    });
-
+    //handles the state of the switch button
     function handleSwitchButton() {
         setSwitchButton(!switchButton);
     }
-
-    function handleFloatValue(event, value) {
-        setSomeFloatValue(value);
+    //handles the state  and value of the switch button
+    function handleSliderFloatValue(event, value) {
+        setSliderFloatValue(value);
     }
+    //handles the functions (asynchronously).
+    //frontend <--> backend communication -- GET/PUT requests are fired
+    //either when there are some changes in this states on the WebApp or when something comes back from the backend
+     useEffect(() => {
+        console.log("Switch button is turned " + (switchButton ? 'on' : 'off'));
+        console.log('Slider value ' + sliderIntegerValue);
+        fetchSwitchButtonData();
+        fetchSwitchButtonPutData();
+        fetchIntegerSliderValueData();
+        fetchIntegerSliderValuePutData();
+        fetchFloatSliderValueData();
+        fetchFloatSliderIntegerValuePutData();
+        fetchInputFieldValueData();
+        fetchInputFieldValuePutData();
+    });
 
+    //Generates and downloads an XML file based on the states and values inside the main form
     function handleSubmit() {
         console.log(switchButton);
-        console.log(sliderValue);
-        console.log(someFloatValue);
+        console.log(sliderIntegerValue);
+        console.log(sliderFloatValue);
 
         var builder = require('xmlbuilder');
         var xml = builder.create('config', { encoding: 'utf-8' })
@@ -156,28 +188,28 @@ export default function Ucu() {
                 'min': '0',
                 'max': '0',
                 'type': 'uint8'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_i8',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'sint8'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_u16',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'uint16'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_i16',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'sint16'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_u32',
                 'init': '0',
@@ -198,7 +230,7 @@ export default function Ucu() {
                 'min': '0',
                 'max': '0',
                 'type': 'float32'
-            }, someFloatValue).up()
+            }, sliderFloatValue).up()
             .ele('element', {
                 'name': 'test_b',
                 'init': '0',
@@ -213,28 +245,28 @@ export default function Ucu() {
                 'min': '0',
                 'max': '0',
                 'type': 'uint8'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_i8',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'sint8'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_u16',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'uint16'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_i16',
                 'init': '0',
                 'min': '0',
                 'max': '0',
                 'type': 'sint16'
-            }, sliderValue).up()
+            }, sliderIntegerValue).up()
             .ele('element', {
                 'name': 'test_u32',
                 'init': '0',
@@ -255,7 +287,7 @@ export default function Ucu() {
                 'min': '0',
                 'max': '0',
                 'type': 'float32'
-            }, someFloatValue).up()
+            }, sliderFloatValue).up()
             .ele('element', {
                 'name': 'test_b',
                 'init': '0',
@@ -266,19 +298,14 @@ export default function Ucu() {
             .end({ pretty: true });
         console.log(xml)
 
-        //we can use File as well but IE and Edge do not support that so it is better to use Blob because it is supported
+        //we can use File instead of Blob. However, neither does IE nor Edge support it.
+        // That is why, is better to use Blob because it is supported on every browser
         var FileSaver = require('file-saver');
         var blob = new Blob([xml], { type: "application/xml;charset=utf-8" });
         FileSaver.saveAs(blob, "AMOS_APP4UCU.xml");
     }
 
-    const style = {
-  
-};
-
-
-
-
+    //HTML rendering of the UI elements
     return (
         <div className="Ucu">
             <h1 className="welcomeHeader">This is the webserver page for the UCU microcontroller. <br /></h1>
@@ -293,22 +320,22 @@ export default function Ucu() {
                 />
                 <label htmlFor="raised-button-file">
                     <Button
-                    style = {{
-                        background: 'linear-gradient(45deg, #be2a92bd 30%, #FF8E53 90%)',
-                        borderRadius: 3,
-                        border: 0,
-                        color: 'white',
-                        height: 48,
-                        padding: '0 30px',
-                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-                    }}
-                     variant="contained" component="span" className="uploadButton">
+                        style={{
+                            background: 'linear-gradient(45deg, #be2a92bd 30%, #FF8E53 90%)',
+                            borderRadius: 3,
+                            border: 0,
+                            color: 'white',
+                            height: 48,
+                            padding: '0 30px',
+                            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                        }}
+                        variant="contained" component="span" className="uploadButton">
                         Upload
                     </Button>
                 </label>
             </div>
             <form onSubmit={handleSubmit}>
-                {showElement ?
+                {showSwitchButton ?
                     <div className="switchButtonDiv">
                         <h2>
                             Turn On and Off<BootstrapSwitchButton
@@ -323,16 +350,21 @@ export default function Ucu() {
 
                     : null
                 }
+
+                {showInputField ?
+                    <div className="normalField">
+                        <h2>
+                            <TextField id="normalField" label="Input Integer Value here" variant="outlined"
+                                InputLabelProps={{ style: { color: '#DBD5D5' }, }} onChange={handleNormalFieldInteger}>
+                            </TextField>
+                        </h2>
+                    </div>
+                    : null
+                }
+
                 {showIntegerSlider ?
                     <div className="Integerslider">
                         <h2>
-                            <div className="normalField">
-                                <h2>
-                                    <TextField id="normalField" label="Input Integer Value here" variant="outlined"
-                                        InputLabelProps={{ style: { color: '#DBD5D5' }, }} onChange={handleNormalFieldInteger}>
-                                    </TextField>
-                                </h2>
-                            </div>
                             <div className="sliderHeader">Slide left or write by clicking on the point
                                 <Slider
                                     defaultValue={30}
@@ -341,7 +373,7 @@ export default function Ucu() {
                                     step={1}
                                     color='primary'
                                     track='inverted'
-                                    onChange={handleChange}
+                                    onChange={handleSliderIntegerValue}
                                     marks={sliderIntValues}
                                     min={0}
                                     max={100}>
@@ -349,45 +381,45 @@ export default function Ucu() {
                             </div>
                         </h2>
                         <div>
-                            <span className="sliderValue">Slider Integer Value {sliderValue}%</span>
+                            <span className="sliderIntegerValue">Slider Integer Value {sliderIntegerValue}%</span>
                         </div>
                     </div>
                     : null
                 }
+
                 {showFloatSlider ?
                     <div className="floatValueDiv">
                         <Slider
                             defaultValue={1.5}
                             aria-labelledby="discrete-slider"
                             valueLabelDisplay="auto"
-                            step={0.1}                                
+                            step={0.1}
                             color='primary'
                             track='inverted'
-                            onChange={handleFloatValue}
-                             marks={sliderFloatValues}
+                            onChange={handleSliderFloatValue}
+                            marks={sliderFloatValues}
                             min={0.0}
                             max={10.0}>
                         </Slider>
                         <div>
-                            <span className="sliderFloatValue">Slider Float Value {someFloatValue}</span>
+                            <span className="sliderFloatValue">Slider Float Value {sliderFloatValue}</span>
                         </div>
                     </div>
                     : null
                 }
-                
 
                 <div className="downloadXML">
-                    <Button 
-                      style = {{
-                        background: 'linear-gradient(45deg, #FF8E53 30%, #be2a92bd 90%)',
-                        borderRadius: 3,
-                        border: 0,
-                        color: 'white',
-                        height: 48,
-                        padding: '0 30px',
-                        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-                    }}
-                    variant="contained"  type={"submit"} color="secondary">
+                    <Button
+                        style={{
+                            background: 'linear-gradient(45deg, #FF8E53 30%, #be2a92bd 90%)',
+                            borderRadius: 3,
+                            border: 0,
+                            color: 'white',
+                            height: 48,
+                            padding: '0 30px',
+                            boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+                        }}
+                        variant="contained" type={"submit"} color="secondary">
                         Download XML
                     </Button>
                 </div>
@@ -395,4 +427,3 @@ export default function Ucu() {
         </div>
     );
 }
-
